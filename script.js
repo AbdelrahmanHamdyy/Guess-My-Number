da23b0553d646afd5e076f4a269f446ba038e9e9
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-07-17 00:36:37 +0200

    DOM Intro

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// DOM (DOCUMENT OBJECT MODEL): Structured representation of html documents that allows JS to access html elements and styles to manipulate them.
+// --> Connection between the html document and the JS code
+// DOM methods are part of (WEB APIs) --> Libraries written in JS
